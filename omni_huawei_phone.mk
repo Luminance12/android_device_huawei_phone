44e963f97b882bcc2778f54eea0e3ba52de52ff2
Luminance12 <37965929+Luminance12@users.noreply.github.com>
2018-09-10 01:18:04 +0200

    Update omni_huawei_phone.mk

--- omni_huawei_phone.mk
+++ omni_huawei_phone.mk
@@ -15,8 +15,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common.mk)
 
-# Inherit device configuration
+TARGET_BOOTANIMATION_SIZE := 1080p
+
+$(call inherit-product, vendor/omni/config/common.mk)
+
 $(call inherit-product, device/huawei/phone/device.mk)
 
 # Device Path
